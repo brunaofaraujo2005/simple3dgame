da11f9a62bea113f9283d570fbe08828d6405a24
robmaas <robmaas@386fa070-3172-11de-ba44-d58fe8a9ce33>
2009-04-25 12:28:29 +0000

    Uitbreiding met de game klasse

---  simple3dgame/week1/week1/TypeDefs.h
+++  simple3dgame/week1/week1/TypeDefs.h
@@ -1,3 +1,17 @@
-//Bevat type definities die door meerdere klasse gebruikt worden
+#pragma once
 
-enum {START, EXIT, SPACE, HOLE, LIFT, WATER, BATTERY, CHIP, WALL};	//Bevat level elementen
+#include <fstream>				
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct position{				//Bevat de positie x, y, platform
+    unsigned int x;
+    unsigned int y;
+    unsigned int platform;
+};
+
+//Om makkelijk te werken een enumeration van veelgebruikte "termen".
+enum {START, EXIT, SPACE, HOLE, LIFT, WATER, BATTERY, CHIP, WALL, NORTH, EAST, SOUTH, WEST};
