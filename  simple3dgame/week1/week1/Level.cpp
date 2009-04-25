d8449098f34219cb13058a080696130acf1c46d9
robmaas <robmaas@386fa070-3172-11de-ba44-d58fe8a9ce33>
2009-04-25 09:28:38 +0000

    Levels inlezen werkt nu

---  simple3dgame/week1/week1/Level.cpp
+++  simple3dgame/week1/week1/Level.cpp
@@ -2,18 +2,18 @@
 
 //Default constructor
 Level::Level(){
+	_numPlatforms = -1;
 };
 
 //Constructor opent een doolhof
 Level::Level(ifstream &mazeStream){
-	//Bestand lezen tot het einde	
-	char chrLine[201];
-	unsigned int step = 0;
-	unsigned int lineNumber = 0;
-	unsigned int platform = 0;
-	unsigned int symbol;
+	char chrLine[201];						//Bevat de regel in chars
+	unsigned int step = 0;					//Houdt bij welke waarde er ingelezen wordt
+	unsigned int lineNumber = 0;			//Houdt het regelnummer bij van het level (Y-as)
+	unsigned int platform = 0;				//Houdt het niveau bij
+	unsigned int symbol;					//Bevat het symbool wat op de betreffende positie staat
 	
-	while(!mazeStream.eof()){
+	while(platform != _numPlatforms){
 		mazeStream.getline(chrLine, 200);
 		if ((chrLine[0] != '%') && (chrLine[0] != 0)){ //Commentaar en lege regels overslaan
 			//Level aanmaken, four steps numplatforms, width, height, platforms
@@ -38,7 +38,6 @@ Level::Level(ifstream &mazeStream){
 							case 'S':
 								symbol = START;
 								_startPosition.platform = platform;
-								_startPosition.symbol = symbol;
 								_startPosition.x = i;
 								_startPosition.y = lineNumber;
 								break;
@@ -65,24 +64,14 @@ Level::Level(ifstream &mazeStream){
 								symbol = SPACE;
 								break;
 						}
-						position point;
-						point.platform = platform;
-						point.y = lineNumber;
-						point.x = i;
-						point.symbol = symbol;
-						_maze.insert(pair<unsigned int, position>(i, point));
+						_maze.push_back(symbol);	//Symbool toevoegen aan vector, wordt automatich geteld.
 					}
 					lineNumber++;
 					//Controleren of het einde van het platform is bereikt.
 					if (lineNumber == _height){
-						//Controleren of nog niet de laatste floor is bereikt.
-						if (platform != _numPlatforms-1){
-							platform++;
-							lineNumber = 0;
+						platform++;
+						lineNumber = 0;
 						}
-						//Hier nog terugkeren naar game
-					}
-					break;
 			}
 		}	
 	}
---  simple3dgame/week1/week1/Level.h
+++  simple3dgame/week1/week1/Level.h
@@ -1,24 +1,32 @@
 #include <fstream>				//Om files te streamen
-#include <map>					//Multimap waar de posities in staan.
+#include <vector>
 
-//TODO: function pointer, zodat ik een map met x,y kan maken
+//TODO: 
 
+/* Level representation in vector
+	########	0  1  2  3  4  5  6  7
+	# C # X#    8  9  10 11 12 13 14 15 etc.
+	# # C ##
+	#C######
+	#  WL  #
+	######C#
+	#S  C  #
+	########
+*/
 using namespace std;
 
 class Level{
 private:
-	struct position{				//Bevat de positie level, x, y, platform
-		unsigned int x;
-		unsigned int y;
-		int platform;
-		unsigned int symbol;
-	};
+	struct position{                                //Bevat de positie x, y, platform
+        unsigned int x;
+        unsigned int y;
+        unsigned int platform;
+    };
 	enum{START, EXIT, SPACE, HOLE, LIFT, WATER, BATTERY, CHIP, WALL};	//Bevat level elementen
 	unsigned int _numPlatforms;		//Aantal aanwezige platforms
 	unsigned int _width, _height;	//Bevat de breedte en hoogte van het level
 	position _startPosition;		//Bevat de startlocatie
-//	map<position, unsigned int> _maze;		//Map waarbij de positie aangeeft, wat er op die positie is
-	multimap<unsigned int, position> _maze;
+	vector<unsigned int> _maze;		//Simpele vector representatie van het level
 public:
 	Level();						//Default constructor
 	Level(ifstream &mazeStrean);	//Constructor die een doolhof inleest.
@@ -28,5 +36,5 @@ public:
 	unsigned int getHeight(){return _height;};
 	unsigned int getStartX(){return _startPosition.x;};
 	unsigned int getStartY(){return _startPosition.y;};
-	int getStartPlatForm(){return _startPosition.platform;};
+	unsigned int getStartPlatForm(){return _startPosition.platform;};
 };
