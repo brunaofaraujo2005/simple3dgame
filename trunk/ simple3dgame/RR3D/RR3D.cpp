5601743ef1def3ccf0a4f08223a0c830b5c59535
robmaas <robmaas@386fa070-3172-11de-ba44-d58fe8a9ce33>
2009-06-20 10:58:03 +0000

    Klein bugje gefixed + testlevel gemaakt ivm fps

--- trunk/ simple3dgame/RR3D/RR3D.cpp
+++ trunk/ simple3dgame/RR3D/RR3D.cpp
@@ -54,7 +54,7 @@ void print(int x, int y, string text, float colorR, float colorG, float colorB){
        glDepthFunc (GL_ALWAYS);
        glColor3f (colorR, colorG, colorB);
        glRasterPos2f(x, y);
-       for (int i = 0; i<text.length(); ++i)
+       for (unsigned int i = 0; i<text.length(); ++i)
                glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, text[i]);
        glDepthFunc (GL_LESS);
        glPopMatrix ();
